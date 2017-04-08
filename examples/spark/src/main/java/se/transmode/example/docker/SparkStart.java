aae9ab8921ffab57ad307b188838e18c1a4b7939
Harshada gothankar <hgharshadagothankar@gmail.com>
2017-04-08 10:36:01 -0400

    wrong code

--- examples/spark/src/main/java/se/transmode/example/docker/SparkStart.java
+++
@@ -1,8 +0,0 @@
-package se.transmode.example.docker;
-import static spark.Spark.get;
-
-public class SparkStart {
-    public static void main(String... args) {
-        get("/", (request, response) -> "<h1>Hello Docker, I'm Spark!</h1>");
-    }
-}
